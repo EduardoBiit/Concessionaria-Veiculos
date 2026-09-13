53c8d0f45527db4f8c9b919d2433b6376e7c3fde
Eduardo <eduardo.399123@gmail.com>
2026-09-13 19:16:04 -0300

    Projeto

---
+++ src/Cliente.java
@@ -0,0 +1,7 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String Classificao;
+}
---
+++ src/veiculo.java
@@ -0,0 +1,8 @@
+public class veiculo {
+    private int id;
+    private String Modelo;
+    private int Ano;
+    private String Marca;
+    private String Cor;
+    private int preco;
+}
