5c433c74ccc62dfd708b89de86676e6f4d5451e9
Eduardo <eduardo.399123@gmail.com>
2026-09-13 18:56:34 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ void main() {
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println("Hello and welcome!");
 
-    for (int i = 1; i <= 7; i++) {
+    for (int i = 1; i <= 10; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        IO.println("i = alo" + i);
     }
 }
