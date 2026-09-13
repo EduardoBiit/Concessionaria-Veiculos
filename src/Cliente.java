a5eced0b7131b74ea6072f72e3d59d1e732201c3
Eduardo <eduardo.399123@gmail.com>
2026-09-13 19:19:35 -0300

    criação e mudança na classe Cliente ( atributos, construtor e getters)

--- src/Cliente.java
+++ src/Cliente.java
@@ -4,4 +4,35 @@ public class Cliente {
     private String telefone;
     private String email;
     private String Classificao;
+
+    /* Construtor Java */
+    public Cliente(int id, String nome, String telefone, String email, String classificao) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        Classificao = classificao;
+    }
+
+    /* Getters para transformar o Private em Public */
+    public int getId() {
+        return id;
+    }
+
+    public String getClassificao() {
+        return Classificao;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
 }
